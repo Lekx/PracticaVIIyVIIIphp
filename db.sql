a044ed9b19a894086abbd946dba7bed27ae119ff
Saúl Sánchez <link_mx@live.com>
2014-04-05 15:12:19 -0600

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,3 @@
+create database prograIV;
+use prograIV;
+create table practica7(id int not null auto_increment, autor char(50) not null, titulo char(30) not null, contenido char(100) not null, primary key(id));
